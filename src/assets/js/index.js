0baea138deeda6a9d1a07826013a9bffb6888cb0
Shketik_R <ruslans3101@gmail.com>
2024-03-28 13:27:02 +0300

    new v

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,161 +1,142 @@
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
-  /*----------------------------Modal-----------------------------*/
+
+  Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
+
   class Modal {
-    constructor(wrap, btn) {
-      this.wrap = typeof wrap === "string" ? document.querySelector(wrap) : wrap;
-      this.btn = typeof btn === "string" ? document.querySelector(btn) : btn;
-      this.menu = this.wrap.firstElementChild;
-      this.#click();
-      this.#closeWindow();
+
+    constructor(option = {}) {
+
+      this.option = option;
+
+      this.ACTIVE_BUTTON = this.option.activeSelectButton ? this.option.activeSelectButton : "active-btn-modal";
+      this.ACTIVE_MODAL = this.option.activeSelectModal ? this.option.activeSelectModal : "visibility-modal";
+      this.BUTTON_DATA_NAME = this.option.buttonDataName ? this.option.buttonDataName : "open-modal";
+      this.MODAL_DATA_NAME = this.option.modalDataName ? this.option.modalDataName : "modal";
+      this.CLOSE_DATA_NAME = this.option.closeDataName ? this.option.closeDataName : "close-modal";
+
+      this.btnAll = document.querySelectorAll(`[data-${this.BUTTON_DATA_NAME}]`);
+      this.modal = document.querySelectorAll(`[data-${this.MODAL_DATA_NAME}]`);
+
+      this.open();
+      this.close();
     }
 
-    #click() {
-      this.btn.addEventListener('click', () => {
-        this.wrap.classList.toggle('active');
-        this.btn.classList.toggle('active');
-        this.wrap.classList.contains('active') ? this.#disableScroll() : this.#enableScroll();
-      })
+    open() {
+
+      if (this.btnAll.length > 0) {
+
+        let newDataName = this._convertString();
+
+        this.btnAll.forEach(b => {
+          b.addEventListener('click', () => {
+            if (b.classList.contains(this.ACTIVE_BUTTON)) {
+              this._removeVisibility();
+              return;
+            }
+            b.classList.toggle(this.ACTIVE_BUTTON);
+            this.requiredModal = document.querySelector(`[data-${this.MODAL_DATA_NAME}="${b.dataset[newDataName]}"]`);
+            if (this.requiredModal) {
+              this.requiredModal.classList.toggle(this.ACTIVE_MODAL);
+              this._disableScroll();
+            }
+          })
+        })
+      }
     }
 
-    #closeWindow() {
-      this.wrap.addEventListener('click', (event) => this.#hide(event));
+    close() {
+      if (this.modal.length > 0) {
+        this.modal.forEach(m => {
+          m.addEventListener('click', (event) => this._hide(event));
+        })
+      }
     }
 
 
-    #hide(event) {
-      if (this.menu.contains(event.target))
-        return;
-      this.wrap.classList.remove('active');
-      this.btn.classList.remove('active');
-      this.#enableScroll();
-      this.wrap.removeEventListener("click", event => this.#hide(event));
+    _hide(event) {
+      this.btnClose = event.currentTarget.querySelectorAll(`[data-${this.CLOSE_DATA_NAME}]`);
+      this.content = event.currentTarget.firstElementChild;
+
+      if (this.btnClose.length > 0) {
+
+        this.btnClose.forEach(close => {
+          if (this.content.contains(event.target) && !close.contains(event.target)) {
+            return;
+          }
+          this._removeVisibility();
+          event.target.removeEventListener("click", event => this._hide(event));
+
+        })
+
+
+      } else {
+        if (this.content.contains(event.target)) {
+          return;
+        }
+        this._removeVisibility();
+        event.target.removeEventListener("click", event => this._hide(event));
+
+      }
     }
 
-    #disableScroll() {
+    _removeVisibility() {
+      document.querySelector('.' + this.ACTIVE_BUTTON) ? document.querySelector('.' + this.ACTIVE_BUTTON).classList.remove(this.ACTIVE_BUTTON) : '';
+      document.querySelector('.' + this.ACTIVE_MODAL) ? document.querySelector('.' + this.ACTIVE_MODAL).classList.remove(this.ACTIVE_MODAL) : '';
+      this._enableScroll();
+    }
+
+    _disableScroll() {
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
       const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-      document.documentElement.classList.add('overflow');
+      document.documentElement.classList.add('lock-scroll');
       window.onscroll = function () {
         window.scrollTo(scrollLeft, scrollTop);
       };
     }
 
-    #enableScroll() {
-      document.documentElement.classList.remove('overflow');
+    _enableScroll() {
+      document.documentElement.classList.remove('lock-scroll');
       window.onscroll = function () { };
     }
-  }
 
-  // const btn = document.querySelector('.open-menu');
-  // const wrap = document.querySelector('.menu-wrapper');
-
-
-  // if (btn && wrap) {
-  //   const menu = new Modal(wrap, btn)
-  // }
-  /*-----------------------*/
-
-
-
-  /*----------------------------DropDownList-----------------------------*/
-  class DropDownList {
-
-    constructor(dropDown, setting) {
-      this.dropDown = dropDown;
-      this.setting = setting
-      this.#getElements();
-      this.#click();
-      if (this.setting.closeWindow === true) {
-        this.#closeWindow()
+    _convertString() {
+      let str = this.BUTTON_DATA_NAME;
+      let i = -1;
+      while ((i = str.indexOf('-', i + 1)) != -1) {
+        str = str.substr(0, i + 1) + str[i + 1].toUpperCase() + str.substr(i + 2);
       }
-    }
-
-    #getElements() {
-      this.#getBtn();
-      this.#getPanel();
-    }
-
-    #getBtn() {
-      return this.btn = typeof this.setting.btn === "string" ? this.dropDown.querySelector(this.setting.btn) : this.setting.btn; //получить кнопки
-    }
-    #getPanel() {
-      return this.panel = typeof this.setting.panel === "string" ? this.dropDown.querySelector(this.setting.panel) : this.setting.panel; // получить панели
-    }
-
-    #click() {
-      this.btn.addEventListener('click', () => {
-        if (this.panel.style.maxHeight) {
-          this.panel.style.maxHeight = null;
-          this.dropDown.classList.remove('active');
-        } else {
-          this.panel.style.maxHeight = this.panel.scrollHeight + "px";
-
-          this.dropDown.classList.add('active');
-        }
-      })
-    }
-    #closeWindow() {
-      this.dropDown.addEventListener('click', () => {
-        document.addEventListener("click", event => {
-          this.#hide(event);
-        });
-      })
-    }
-    #hide(event) {
-      if (this.dropDown.contains(event.target))
-        return;
-      this.panel.style.maxHeight = null;
-      this.dropDown.classList.remove('active');
-      document.removeEventListener("click", event => {
-        this.#hide(event);
-      });
+      str = str.replace(/[\.\-/\\\s]/g, '');
+      return str
     }
   }
 
-
-  // let dropdown = document.querySelectorAll('.dropdown');
-
-  // if (dropdown.length > 0) {
-  //   dropdown.forEach(dd => {
-  //     new DropDownList(dd, {
-  //       panel: '.dropdown__panel',
-  //       btn: '.dropdown__btn',
-  //       closeWindow: true, // не обязательно, закрывать панель при клике вне блока
-  //     })
-
-  //   })
-  // }
-  /*-----------------------*/
-
-
-  /*----------------------------Accordion-----------------------------*/
   class Accordion {
-
-
     constructor(accordion, setting) {
 
       this.accordion = typeof accordion === "string" ? document.querySelector(accordion) : accordion;
       this.setting = setting;
-      this.#getElements();
-      this.#click();
-      this.#showPanel();
+      this._getElements();
+      this.click();
+      this._showPanel();
     }
 
-    #getElements() {
-      this.#getBtn();
-      this.#getPanel();
+    _getElements() {
+      this._getBtn();
+      this._getPanel();
     }
 
-    #getBtn() {
-      return this.btnAll = typeof this.setting.btn === "string" ? this.accordion.querySelectorAll(this.setting.btn) : this.setting.btn;  //получить все кнопки
+    _getBtn() {
+      return this.btnAll = typeof this.setting.btn === "string" ? this.accordion.querySelectorAll(this.setting.btn) : this.setting.btn;
     }
-    #getPanel() {
+    _getPanel() {
       return this.panelAll = typeof this.setting.panel === "string" ? this.accordion.querySelectorAll(this.setting.panel) : this.setting.panel; // получить все панели
     }
 
-
-    #click() {
+    click() {
       this.btnAll.forEach((el, id) => {
         el.addEventListener('click', () => {
           //если true показывать только одну панель 
@@ -166,13 +147,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
               return;
             }
             this.panelAll.forEach((p, id) => {
+              p.style.maxHeight = p.scrollHeight + "px";
               p.style.maxHeight = null;
+
               this.btnAll[id].classList.remove('active');
             });
             this.panelAll[id].style.maxHeight = this.panelAll[id].scrollHeight + "px";
             this.btnAll[id].classList.add('active');
+
+
+
           } else {
             if (this.panelAll[id].style.maxHeight) {
+
               this.panelAll[id].style.maxHeight = null;
               this.btnAll[id].classList.remove('active');
             } else {
@@ -184,7 +171,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
       });
     }
 
-    #showPanel() {
+    _showPanel() {
       // открытая панель
       if (this.setting.showPanel) {
         const counter = this.setting.showPanel - 1;
@@ -198,134 +185,111 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   }
 
+  class DoubleAccordion extends Accordion {
+    click() {
+      this.btnAll.forEach((el, id) => {
+        el.addEventListener('click', () => {
+          if (this.setting.onlyOnePanel === true) {
+            if (this.panelAll[id].style.maxHeight) {
 
+              this.panelAll[id].style.maxHeight = null;
+              this.btnAll[id].classList.remove('active');
+              return;
+            }
+            this.panelAll.forEach((p, id) => {
+              p.style.maxHeight = p.scrollHeight + "px";
+              p.style.maxHeight = null;
 
-  // new Accordion('.accordion', {
-  //   panel: '.accordion__panel',
-  //   btn: '.accordion__btn',
-  //   onlyOnePanel: true,        // открывать только одну
-  //   showPanel: 1,               // выбираем открытую панель
-  // })
-  /*-----------------------*/
-
-
-
-
-  /*----------------------------Prelouder-----------------------------*/
-  // let promise = new Promise((resolve) => {
-  //   setTimeout(() => {
-  //     document.body.classList.add('loaded_hiding');
-  //     resolve();
-  //   }, 1800);
-
-  // });
-  // promise
-  //   .then(
-  //     result => {
-  //       setTimeout(() => {
-  //         document.body.classList.add('loaded');
-  //       }, 500);
-  //     }
-  //   );
-  /*-----------------------*/
-
-  /*-------------------------------Swipper------------------------------*/
-  // new Swiper('.swiper', {
-  //   loop: true,
-  //   speed:900,
-  //   navigation: {
-  //     nextEl: '.btn-next',
-  //     prevEl: '.btn-prev',
-  //   },
-  //   breakpoints: {
-  //     640: {
-  //       slidesPerView: 1,
-  //     },
-  //     1024: {
-  //       slidesPerView: 3,
-  //     },
-  //   },
-  // });
-  /*-----------------------*/
+              this.btnAll[id].classList.remove('active');
+            });
+            this.panelAll[id].style.maxHeight = '100vh';
+            this.btnAll[id].classList.add('active');
 
 
 
+          } else {
+            if (this.panelAll[id].style.maxHeight) {
 
-  function disableScroll() {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-    document.documentElement.scrollLeft;
-    document.documentElement.style.setProperty('scroll-behavior', 'hidden');
-    document.documentElement.classList.add('scroll-lock');
-
-    window.onscroll = function () {
-      window.scrollTo(scrollLeft, scrollTop);
-    };
-  }
-  function enableScroll() {
-    document.documentElement.style.setProperty('scroll-behavior', null);
-    document.documentElement.classList.remove('scroll-lock');
-    window.onscroll = function () { };
+              this.panelAll[id].style.maxHeight = null;
+              this.btnAll[id].classList.remove('active');
+            } else {
+              this.panelAll[id].style.maxHeight = '100vh';
+              this.btnAll[id].classList.add('active');
+            }
+          }
+        })
+      });
+    }
   }
 
+  new Modal();
+
+  const Anchors = {
+    anchors: document.querySelectorAll("a[href*='#']"),
+    btn: document.querySelectorAll("[data-scroll]"),
+    top: 100,
+
+    scrollIntoView() {
+      if (this.anchors.length > 0) {
+        for (let anchor of this.anchors) {
+          anchor.addEventListener('click',  (ev) => {
+            ev.preventDefault();
+            const blockID = anchor.getAttribute('href').substr(1);
+            const block = document.getElementById(blockID);
+            if (block) {
+              block.style.scrollMarginTop = `${this.top}px`
+              block.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+              })
+            }
+          })
+        }
+      }
+    },
+
+    clickButtonScroll() {
+
+      if (this.btn.length > 0) {
+        for (let b of this.btn) {
+          b.addEventListener('click',  (ev) => {
+            ev.preventDefault();
+            const id = ev.currentTarget.dataset.scroll;
+            const bl = document.getElementById(id);
+            
+            if (bl) {
+              bl.style.scrollMarginTop = `${this.top}px`
+            
+              bl.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+              })
+            }
+          })
+        }
+      }
+    }
+
+  }
+
+  Anchors.scrollIntoView();
+  if (window.innerWidth < 768) {
+    Anchors.clickButtonScroll();
+  }
+
+
+  const form = document.querySelectorAll('form');
+
+  if (form) {
+    form.forEach(f => {
+      f.addEventListener('submit', (e) => {
+        e.preventDefault();
+      })
+    })
+  }
 
 
 
-
-  /*-------------------------------animation------------------------------*/
-
-  // function onEntry(entry) {
-  //   entry.forEach(change => {
-  //     if (change.isIntersecting) {
-  //       change.target.classList.add('element-show');
-
-  //     } else { change.target.classList.remove('element-show'); }
-  //   });
-  // }
-  // let options = {
-  //   threshold: [0.3]
-  // };
-
-  // let observer = new IntersectionObserver(onEntry, options);
-  // let elements = document.querySelectorAll('.element-animation');
-  // let elementsAnimationRight = document.querySelectorAll('.element-animation-right');
-  // let elementsAnimationLeft = document.querySelectorAll('.element-animation-left');
-  // let elementsAnimationOpacitty = document.querySelectorAll('.element-animation-opacity')
-
-  // function animationElement(elements) {
-  //   for (let elm of elements) {
-  //     observer.observe(elm);
-  //   }
-  // };
-
-  // if (elements) {
-  //   animationElement(elements);
-  // }
-  // if (elementsAnimationOpacitty) {
-  //   animationElement(elementsAnimationOpacitty);
-  // }
-
-  // if (elementsAnimationRight) {
-  //   animationElement(elementsAnimationRight);
-  // }
-  // if (elementsAnimationLeft) {
-  //   animationElement(elementsAnimationLeft);
-  // }
-  /*-----------------------*/
-
-  /*---------------------------------form---------------------------------*/
-  // let formContract = document.querySelectorAll('form')
-  // if (formContract) {
-  //   formContract.forEach(f => {
-  //     f.addEventListener("submit", (e) => {
-  //       e.preventDefault();
-  //     })
-  //   })
-  // }
-  /*-----------------------*/
 
 
 });
-
-
-
